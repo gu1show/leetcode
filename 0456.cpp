8a031dae5745621008c912c4e6dda0ec37d89dcd
gu1show <denissolovyov57@gmail.com>
2022-05-07 23:58:47 +0300

    0456

---
+++ 0456.cpp
@@ -0,0 +1,32 @@
+// Link: https://leetcode.com/problems/132-pattern/
+// Runtime: 85 ms
+// Memory: 38.2 MB
+
+class Solution 
+{
+public:
+    bool find132pattern(const std::vector<int>& nums)
+    {
+        int thirdElement = INT_MIN;
+        
+        int i = nums.size() - 1;
+        bool isFound = false;
+        while ((i > -1) && (!isFound))
+        {
+            if ((nums[i] < thirdElement))  
+                isFound = true;
+            while ((!pattern.empty()) && (pattern.top() < nums[i]))
+            {
+                thirdElement = pattern.top();
+                pattern.pop();
+            }
+            pattern.push(nums[i]);
+            
+            i--;
+        }
+        return isFound;
+    }
+
+private:
+    std::stack<int> pattern;
+};
