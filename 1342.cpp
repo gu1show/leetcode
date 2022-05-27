6de5259aa6d3af9e32881120d916245e175e3a33
gu1show <denissolovyov57@gmail.com>
2022-05-27 23:33:30 +0300

    1342

---
+++ 1342.cpp
@@ -0,0 +1,19 @@
+// Link: https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+// Runtime: 3 ms
+// Memory: 5.9 MB
+
+class Solution 
+{
+public:
+    int numberOfSteps(int num) 
+    {
+        int steps = 0;
+        while (num > 0)
+        {
+            if (num % 2 == 0) num = num >> 1;
+            else --num;
+            ++steps;
+        }
+        return steps;
+    }
+};
