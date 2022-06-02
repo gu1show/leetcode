064e218763795b0f542d9cf669e50fadac30fa23
gu1show <denissolovyov57@gmail.com>
2022-06-02 10:17:24 +0300

    Add a new solution for 1480

--- 1480.cpp
+++ 1480.cpp
@@ -1,4 +1,5 @@
 // Link: https://leetcode.com/problems/running-sum-of-1d-array/
+// The first solution
 // Runtime: 5 ms 
 // Memory: 8.5 MB
 
@@ -10,6 +11,22 @@ public:
         for (int i = nums.size() - 1; i > -1; i--)
             for (int j = 0; j < i; j++)
                 nums[i] += nums[j];
+        
+        return nums;
+    }
+};
+
+// The second solution
+// Runtime: 0 ms
+// Memory: 8.5 MB
+
+class Solution 
+{
+public:
+    std::vector<int> runningSum(std::vector<int>& nums)
+    {
+        for (int i = 1; i < nums.size(); i++)
+            nums[i] += nums[i - 1];
         
         return nums;
     }
