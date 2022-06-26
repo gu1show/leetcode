3d633769feef47ef20e826fbc1d79b299ad0b132
gu1show <denissolovyov57@gmail.com>
2022-06-26 19:23:15 +0300

    1423

---
+++ 1423.cpp
@@ -0,0 +1,24 @@
+// Link: https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/
+// Runtime: 93 ms
+// Memory: 42.4 MB
+
+class Solution 
+{
+public:
+    int maxScore(std::vector<int>& cardPoints, int k) 
+    {
+        int leftSum = 0;
+        for (int i = 0; i < k; i++) leftSum += cardPoints[i];
+        
+        int windowSum = leftSum;
+        int result = leftSum;
+
+        for (int j = k - 1; j > -1; j--)
+        {
+            windowSum = windowSum - cardPoints[j] + cardPoints[cardPoints.size() + j - k];
+            result = std::max(result, windowSum);
+        }
+        
+        return result;
+    }
+};
