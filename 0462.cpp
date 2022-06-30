46eb765a329fa76b39838c4862ff71e74907070d
gu1show <denissolovyov57@gmail.com>
2022-06-30 14:50:35 +0300

    0462

---
+++ 0462.cpp
@@ -0,0 +1,38 @@
+// Link: https://leetcode.com/problems/minimum-moves-to-equal-array-elements-ii/
+// The first solution
+// Runtime: 24 ms
+// Memory: 10.8 MB
+
+class Solution 
+{
+public:
+    int minMoves2(std::vector<int>& nums) 
+    {
+        std::sort(nums.begin(), nums.end());
+        
+        int middleValue = nums[nums.size() / 2], minimumMoves = 0;
+        for (int i = 0; i < nums.size(); i++)
+            minimumMoves += abs(nums[i] - middleValue);
+        
+        return minimumMoves;
+    }
+};
+
+// The second solution
+// Runtime: 11 ms
+// Memory: 10.9 MB
+
+class Solution 
+{
+public:
+    int minMoves2(std::vector<int>& nums) 
+    {
+        std::nth_element(nums.begin(), nums.begin() + nums.size() / 2, nums.end());
+        
+        int middleValue = nums[nums.size() / 2], minimumMoves = 0;
+        for (int i = 0; i < nums.size(); i++)
+            minimumMoves += abs(nums[i] - middleValue);
+        
+        return minimumMoves;
+    }
+};
