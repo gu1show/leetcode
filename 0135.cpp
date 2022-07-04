a23ab928aa2d107db08a6b31db1646d016806824
gu1show <denissolovyov57@gmail.com>
2022-07-05 00:11:00 +0300

    0135

---
+++ 0135.cpp
@@ -0,0 +1,25 @@
+// Link: https://leetcode.com/problems/candy/
+// Runtime: 43 ms
+// Memory: 18.3 MB
+
+class Solution 
+{
+public:
+    int candy(const std::vector<int>& ratings) 
+    {
+        int size = ratings.size();
+        std::vector<int> leftToRight(size, 1), rightToLeft(size, 1);        
+        
+        for (int i = 0; i < size - 1; i++)
+            if (ratings[i] < ratings[i + 1]) leftToRight[i + 1] = leftToRight[i] + 1; 
+        
+        for (int i = size - 2; i > -1; i--)
+            if (ratings[i] > ratings[i + 1]) rightToLeft[i] = rightToLeft[i + 1] + 1;
+        
+        int sum = 0;
+        for (int i = 0; i < size; i++)
+            sum += std::max(leftToRight[i], rightToLeft[i]);
+        
+        return sum;
+    }
+};
