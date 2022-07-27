c00848f88f8397ddd77d6f3830278d7c2e1b835b
gu1show <denissolovyov57@gmail.com>
2022-07-27 14:29:30 +0300

    0114

---
+++ 0114.cpp
@@ -0,0 +1,47 @@
+// Link: https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+// Runtime: 15 ms
+// Memory: 12.7 MB
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution 
+{
+public:
+    void flatten(TreeNode* root) 
+    {
+        if (root != nullptr)
+        {
+            TreeNode* nextRightNode = nullptr;
+            TreeNode* rightNodeMost = nullptr;
+            
+            while (root != nullptr)
+            {
+                if (root->left != nullptr)
+                {
+                    rightNodeMost = getMostRightNode(root->left);
+                    nextRightNode = root->right;
+                    root->right = root->left;
+                    root->left = nullptr;
+                    rightNodeMost->right = nextRightNode;
+                }  
+                root = root->right;
+            }
+        }
+    }
+
+private:
+    TreeNode* getMostRightNode(TreeNode* node)
+    {
+        if (node->right == nullptr) return node;
+        else return getMostRightNode(node->right);
+    }
+};
