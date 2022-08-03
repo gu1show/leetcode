af7b7ae27ac96b349f6d502fc44c8d6ead2594db
gu1show <denissolovyov57@gmail.com>
2022-08-03 21:22:55 +0300

    0729

---
+++ 0729.cpp
@@ -0,0 +1,33 @@
+// Link: https://leetcode.com/problems/my-calendar-i/
+// Runtime: 166 ms
+// Memory: 38.9 MB
+
+class MyCalendar 
+{
+public:
+    MyCalendar()
+    {
+        
+    }
+    
+    bool book(int start, int end) 
+    {
+        auto it = calender.upper_bound(start);
+        if ((it == calender.end()) || (it->second >= end)) 
+        {
+            calender[end] = start;
+            return true;
+        }
+        else return false;
+    }
+    
+private:
+    std::map<int, int> calender;
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
+ 
