ef69c898d774b8371eda7946a7c49774921cc8c4
gu1show <denissolovyov57@gmail.com>
2022-08-06 17:08:36 +0300

    0458

---
+++ 0458.cpp
@@ -0,0 +1,12 @@
+// Link: https://leetcode.com/problems/poor-pigs/
+// Runtime: 0 ms 
+// Memory: 6 MB
+
+class Solution 
+{
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) 
+    {
+        return std::ceil(std::log(buckets) / std::log(minutesToTest / minutesToDie + 1));
+    }
+};
