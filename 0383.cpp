b7b82a7f8715d705ad09173a4223a2b1a1c78f6b
gu1show <denissolovyov57@gmail.com>
2022-08-25 20:37:17 +0300

    0383

---
+++ 0383.cpp
@@ -0,0 +1,27 @@
+// Link: https://leetcode.com/problems/ransom-note/
+// Runtime: 45 ms 
+// Memory: 8.1 MB
+
+class Solution 
+{
+public:
+    bool canConstruct(const std::string& ransomNote, const std::string& magazine) 
+    {
+        std::unordered_map<char, int> countingChars;
+        for (const auto& c : magazine)
+            countingChars[c]++;
+        
+        for (const auto& c : ransomNote)
+            countingChars[c]--;
+        
+        bool containAll = true;
+        auto it = countingChars.begin();
+        while ((it != countingChars.end()) && (containAll))
+        {
+            if (it->second < 0) containAll = false;
+            ++it;
+        }
+        
+        return containAll;
+    }
+};
