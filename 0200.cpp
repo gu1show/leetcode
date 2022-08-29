15df4cc6fc985bea1191a1a3f0e55374d0dfcd98
gu1show <denissolovyov57@gmail.com>
2022-08-29 22:45:35 +0300

    0200

---
+++ 0200.cpp
@@ -0,0 +1,35 @@
+// Link: https://leetcode.com/problems/number-of-islands/
+// Runtime: 53 ms 
+// Memory: 12.2 MB
+
+class Solution 
+{
+public:
+    int numIslands(std::vector<std::vector<char>>& grid) 
+    {
+        int numberOfIslands = 0;
+        for (int i = 0; i < grid.size(); i++)
+            for (int j = 0; j < grid[0].size(); j++)
+                if (grid[i][j] == '1')
+                {
+                    processIsland(grid, i, j);
+                    numberOfIslands++; 
+                }
+        
+        return numberOfIslands;
+    }
+    
+private:
+    void processIsland(std::vector<std::vector<char>>& grid, int i, int j)
+    {
+        if ((i > -1) && (i < grid.size()) && (j > -1) && (j < grid[0].size()) && (grid[i][j] == '1'))
+        {
+            grid[i][j] = '0';
+            
+            processIsland(grid, i - 1, j);
+            processIsland(grid, i + 1, j);
+            processIsland(grid, i, j - 1);
+            processIsland(grid, i, j + 1);
+        }
+    }
+};
