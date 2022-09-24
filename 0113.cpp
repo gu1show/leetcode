12f1530f969f64b19567309369f0443aed7c3e6d
gu1show <denissolovyov57@gmail.com>
2022-09-24 18:17:03 +0300

    0113

---
+++ 0113.cpp
@@ -0,0 +1,45 @@
+// Link: https://leetcode.com/problems/path-sum-ii/
+// Runtime: 15 ms
+// Memory: 38.8 MB
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution 
+{
+public:
+    std::vector<std::vector<int>> pathSum(TreeNode* root, int targetSum) 
+    {
+        std::vector<int> way;
+        addPaths(root, way, targetSum);
+        
+        return paths;
+    }
+    
+private:
+    std::vector<std::vector<int>> paths;
+    
+    void addPaths(TreeNode* root, std::vector<int> way, int currentSum)
+    {
+        if (root != nullptr)
+        {
+            currentSum -= root->val;
+            way.push_back(root->val);
+            if ((currentSum == 0) && (root->left == nullptr) && (root->right == nullptr))
+                paths.push_back(way);
+            else
+            {           
+                addPaths(root->left, way, currentSum);
+                addPaths(root->right, way, currentSum);
+            }
+        }   
+    }
+};
