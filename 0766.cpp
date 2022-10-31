594328dd8e653274f0df29d0e3d49ea383588961
gu1show <denissolovyov57@gmail.com>
2022-10-31 15:27:26 +0300

    0766

---
+++ 0766.cpp
@@ -0,0 +1,17 @@
+// Link: https://leetcode.com/problems/toeplitz-matrix/
+// Runtime: 40 ms
+// Memory: 17.4 MB
+
+class Solution 
+{
+public:
+    bool isToeplitzMatrix(const std::vector<std::vector<int>>& matrix)
+    {
+        for (int i = 0; i < static_cast<int>(matrix.size()); i++)
+            for (int j = 0; j < static_cast<int>(matrix[0].size()); j++)
+                if ((i > 0) && (j > 0) && (matrix[i][j] != matrix[i - 1][j - 1]))
+                    return false;
+        
+        return true;
+    }
+};
