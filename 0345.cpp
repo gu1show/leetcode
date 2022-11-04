c8851ba3e3a219fddaf56489f65525aad22b8350
gu1show <denissolovyov57@gmail.com>
2022-11-04 23:09:56 +0300

    0345

---
+++ 0345.cpp
@@ -0,0 +1,43 @@
+// Link: https://leetcode.com/problems/reverse-vowels-of-a-string/description/
+// Runtime: 14 ms
+// Memory: 8.2 MB
+
+class Solution 
+{
+public:
+    std::string reverseVowels(const std::string& s)
+    {
+        std::string copy = s;
+        setVowels();
+        int left = 0, right = static_cast<int>(s.size() - 1);
+        while (left < right)
+            if ((vowels.find(copy[left]) != vowels.end()) &&
+                (vowels.find(copy[right]) != vowels.end()))
+            {
+                std::swap(copy[left], copy[right]);
+                left++;
+                right--;
+            }
+            else if (vowels.find(copy[left]) != vowels.end()) right--;
+            else left++;
+
+        return copy;
+    }
+
+private:
+    std::set<char> vowels;
+
+    void setVowels() 
+    {
+        vowels.insert('a');
+        vowels.insert('e');
+        vowels.insert('i');
+        vowels.insert('o');
+        vowels.insert('u');
+        vowels.insert('A');
+        vowels.insert('E');
+        vowels.insert('I');
+        vowels.insert('O');
+        vowels.insert('U');
+    }
+};
