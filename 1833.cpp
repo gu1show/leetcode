41d6f9b93e628c1a43d6dd536277d1460f9c0417
gu1show <denissolovyov57@gmail.com>
2023-01-06 13:11:04 +0300

    1833

---
+++ 1833.cpp
@@ -0,0 +1,22 @@
+// Link: https://leetcode.com/problems/maximum-ice-cream-bars/description/
+// Runtime: 199 ms 
+// Memory: 79.9 MB
+
+class Solution 
+{
+public:
+    int maxIceCream(std::vector<int> costs, int coins) 
+    {
+        std::sort(costs.begin(), costs.end());
+
+        int i = 0, corns = 0;
+        while ((i < static_cast<int>(costs.size())) && (coins >= costs[i])) 
+        {
+            coins -= costs[i];
+            corns++;
+            i++;
+        }
+
+        return corns;
+    }
+};
