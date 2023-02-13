3d528952863db8ff979752c8bf07758b7c273da0
gu1show <denissolovyov57@gmail.com>
2023-02-13 23:41:09 +0300

    1523

---
+++ 1523.cpp
@@ -0,0 +1,13 @@
+// Link: https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/description/
+// Runtime: 0 ms
+// Memory: 6.1 MB
+
+class Solution 
+{
+public:
+    int countOdds(int low, int high) 
+    {
+        if ((low % 2 == 0) && (high % 2 == 0)) return (high - low) / 2;
+        else return (high - low) / 2 + 1;
+    }
+};
