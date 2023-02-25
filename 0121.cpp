65ad451fd7e5b9d16ff32b3baef0f3ef64887225
gu1show <denissolovyov57@gmail.com>
2023-02-25 16:07:21 +0300

    0121

---
+++ 0121.cpp
@@ -0,0 +1,19 @@
+// Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+// Runtime: 127 ms
+// Memory: 93.3 MB
+
+class Solution 
+{
+public:
+    int maxProfit(const std::vector<int>& prices) 
+    {
+        int priceForBuying = prices[0], profit = 0;
+        for (const int& price : prices) 
+        {
+            if (price < priceForBuying) priceForBuying = price;
+            if (profit < price - priceForBuying) profit = price - priceForBuying;
+        }
+
+        return profit;
+    }
+};
