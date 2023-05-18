1d4ccfff1f5c58d8837fbb06f7541cc42ae4a26e
gu1show <denissolovyov57@gmail.com>
2023-05-18 23:58:37 +0300

    1557

---
+++ python/1557.py
@@ -0,0 +1,16 @@
+# Link: https://leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes/description/
+# Runtime: 1157 ms
+# Memory: 55 MB
+
+class Solution:
+    def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        waysToNodes = [0] * n
+        for edge in edges:
+            waysToNodes[edge[1]] += 1
+        
+        unreachableNodes = []
+        for index, value in enumerate(waysToNodes):
+            if value == 0:
+                unreachableNodes.append(index)
+        
+        return unreachableNodes
