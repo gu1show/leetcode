7d8cd0da6b4055515baf26b4e401fec9ae1a4577
gu1show <denissolovyov57@gmail.com>
2023-07-14 22:19:35 +0300

    1218

---
+++ c++/1218.cpp
@@ -0,0 +1,20 @@
+// Link: https://leetcode.com/problems/longest-arithmetic-subsequence-of-given-difference/description/
+// Runtime: 151 ms
+// Memory: 56.52 MB
+
+class Solution 
+{
+public:
+    int longestSubsequence(const std::vector<int>& arr, int difference) 
+    {
+        int maxSubsequence = 1;
+        std::unordered_map<int, int> lengthOfSubsequenceEndedByValue;
+        for (const auto& val : arr)
+        {
+            lengthOfSubsequenceEndedByValue[val] = lengthOfSubsequenceEndedByValue[val - difference] + 1;
+            maxSubsequence = std::max(maxSubsequence, lengthOfSubsequenceEndedByValue[val]);
+        }
+
+        return maxSubsequence;
+    }
+};
