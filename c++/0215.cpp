0e0a598a436c9d8882bf7fa78e2e23b34f69412f
gu1show <denissolovyov57@gmail.com>
2023-08-14 12:15:28 +0300

    0215

---
+++ c++/0215.cpp
@@ -0,0 +1,20 @@
+// Link: https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+// Runtime: 82 ms
+// Memory: 47.3 MB
+
+class Solution 
+{
+public:
+    int findKthLargest(const std::vector<int>& nums, int k) 
+    {
+        std::priority_queue<int> sortedElements {std::less<int>(), nums};
+        int i = 0;
+        while (i < k - 1) 
+        {
+            sortedElements.pop();
+            i++;
+        }
+
+        return sortedElements.top();
+    }
+};
---
+++ python/0215.py
@@ -0,0 +1,21 @@
+# Link: https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+# The first solution
+# Runtime: 525 ms
+# Memory: 33.5 MB
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        return heapq.nlargest(k, nums)[-1]
+
+# The second solution
+# Runtime: 493 ms
+# Memory: 29.28 MB
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
+        for val in nums:
+            heapq.heappush(heap, val)
+            if len(heap) > k: heapq.heappop(heap)
+        
+        return heap[0]
