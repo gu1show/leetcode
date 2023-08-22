3ff04ca1091c88f91d536d8945b34203019de72a
gu1show <denissolovyov57@gmail.com>
2023-08-22 23:05:41 +0300

    0168

---
+++ c++/0168.cpp
@@ -0,0 +1,21 @@
+// Link: https://leetcode.com/problems/excel-sheet-column-title/description/
+// Runtime: 2 ms
+// Memory: 5.91 MB
+
+class Solution 
+{
+public:
+    std::string convertToTitle(int columnNumber) 
+    {
+        int number = columnNumber;
+        std::string columnTitle = "";
+        while (number > 0)
+        {
+            number--;
+            columnTitle = static_cast<char>('A' + number % 26) + columnTitle;
+            number /= 26;
+        }
+
+        return columnTitle;
+    }
+};
---
+++ python/0168.py
@@ -0,0 +1,14 @@
+# Link: https://leetcode.com/problems/excel-sheet-column-title/description/
+# Runtime: 34 ms
+# Memory: 16.34 MB
+
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        number = columnNumber
+        column_title = ""
+        while number != 0:
+            number -= 1
+            column_title = chr(ord('A') + number % 26) + column_title
+            number //= 26
+
+        return column_title
