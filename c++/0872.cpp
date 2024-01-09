6b2ecf992b5767f8323c0428ec2dffd118373226
gu1show <denissolovyov57@gmail.com>
2024-01-09 13:08:08 +0300

    0872

---
+++ c++/0872.cpp
@@ -0,0 +1,41 @@
+// Link: https://leetcode.com/problems/leaf-similar-trees/description/
+// Runtime: 3 ms 
+// Memory: 13.26 MB
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution 
+{
+public:
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) 
+    {
+        std::vector<int> firstTreeLeafs, secondTreeLeafs;
+
+        getLeafs(root1, firstTreeLeafs);
+        getLeafs(root2, secondTreeLeafs);
+
+        return firstTreeLeafs == secondTreeLeafs;
+    }
+
+private:
+    void getLeafs(TreeNode* root, std::vector<int>& leafs)
+    {
+        if (root != nullptr)
+        {
+            if ((root->left == nullptr) && (root->right == nullptr))
+                leafs.push_back(root->val);
+            
+            getLeafs(root->left, leafs);
+            getLeafs(root->right, leafs);
+        }
+    }
+};
---
+++ python/0872.py
@@ -0,0 +1,21 @@
+# Link: https://leetcode.com/problems/leaf-similar-trees/description/
+# Runtime: 42 ms
+# Memory: 17.44 MB
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        def dfs(node):
+            if node:
+                if (not node.left) and (not node.right):
+                    yield node.val
+                
+                yield from dfs(node.left)
+                yield from dfs(node.right)
+            
+        return list(dfs(root1)) == list(dfs(root2))
