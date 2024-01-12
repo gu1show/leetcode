61dd4fa30d5481a93d9a0212c06f51ddb35e2d78
gu1show <denissolovyov57@gmail.com>
2024-01-12 23:55:06 +0300

    1704

--- c++/1704.cpp
+++ c++/1704.cpp
@@ -1,4 +1,5 @@
 // Link: https://leetcode.com/problems/determine-if-string-halves-are-alike/description/
+// The first solution
 // Runtime: 8 ms
 // Memory: 6.7 MB
 
@@ -24,5 +25,35 @@ private:
                 numberOfVowels++;
         
         return numberOfVowels;
+    }
+};
+
+// The second solution
+// Runtime: 4 ms
+// Memory: 6.82 MB
+
+class Solution 
+{
+public:
+    bool halvesAreAlike(const std::string& s) 
+    {
+        int numberOfVowelsInFirstPart = 0, numberOfVowelsInSecondPart = 0;
+        for (int i = 0; i < static_cast<int>(s.size() / 2); i++)
+            if ((s[i] == 'a') || (s[i] == 'A') || 
+                (s[i] == 'e') || (s[i] == 'E') || 
+                (s[i] == 'i') || (s[i] == 'I') || 
+                (s[i] == 'o') || (s[i] == 'O') || 
+                (s[i] == 'u') || (s[i] == 'U'))
+                numberOfVowelsInFirstPart++;
+
+        for (int i = static_cast<int>(s.size() / 2); i < static_cast<int>(s.size()); i++)
+            if ((s[i] == 'a') || (s[i] == 'A') || 
+                (s[i] == 'e') || (s[i] == 'E') || 
+                (s[i] == 'i') || (s[i] == 'I') || 
+                (s[i] == 'o') || (s[i] == 'O') || 
+                (s[i] == 'u') || (s[i] == 'U'))
+                numberOfVowelsInSecondPart++;
+
+        return numberOfVowelsInFirstPart == numberOfVowelsInSecondPart;
     }
 };
