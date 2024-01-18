70fd3c1e625552992d18a9cfd0c65eaf84fb5f86
gu1show <denissolovyov57@gmail.com>
2024-01-18 22:22:17 +0300

    0070

---
+++ c++/0070.cpp
@@ -0,0 +1,20 @@
+// Link: https://leetcode.com/problems/climbing-stairs/description/
+// Runtime: 0 ms
+// Memory: 6.47 MB
+
+class Solution 
+{
+public:
+    int climbStairs(int n) 
+    {
+        long long oneBefore = 1, twoBefore = 1;
+        for (int i = 0; i < n; i++)
+        {
+            long long temp = oneBefore;
+            oneBefore += twoBefore;
+            twoBefore = temp; 
+        }
+
+        return twoBefore;
+    }
+};
