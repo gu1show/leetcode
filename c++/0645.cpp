2d1f217d335dd28fa2b74785c43479c6c7ccf44d
gu1show <denissolovyov57@gmail.com>
2024-01-22 19:33:22 +0300

    0645

--- c++/0645.cpp
+++ c++/0645.cpp
@@ -1,27 +1,31 @@
-// Link: https://leetcode.com/problems/set-mismatch/
-// Runtime: 73 ms
-// Memory: 21.2 MB
+// Link: https://leetcode.com/problems/set-mismatch/description/
+// Runtime: 36 ms
+// Memory: 32.22 MB
 
 class Solution 
 {
 public:
-    std::vector<int> findErrorNums(std::vector<int>& nums) 
+    std::vector<int> findErrorNums(const std::vector<int>& nums) 
     {
-        std::sort(nums.begin(), nums.end());
-        
-        std::vector<int> duplicateAndRightValue(2);
-        duplicateAndRightValue[1] = 1;
-        for (int i = 1; i < static_cast<int>(nums.size()); i++)
+        int expectedSum = nums.size() * (nums.size() + 1) / 2;
+        int actualSum = std::reduce(nums.begin(), nums.end());
+
+        std::unordered_set<int> differentNumbers;
+        int i = 0, duplicate = 0;
+        bool isFound = false;
+        while ((i < static_cast<int>(nums.size())) && (!isFound))
         {
-            if (nums[i] == nums[i - 1])
-                duplicateAndRightValue[0] = nums[i];
-            else if (nums[i] > nums[i - 1] + 1)
-                duplicateAndRightValue[1] = nums[i - 1] + 1;
+            if (differentNumbers.contains(nums[i]))
+            {
+                duplicate = nums[i];
+                isFound = true;
+            }
+
+            differentNumbers.insert(nums[i]);
+
+            i++;
         }
-        
-        if (nums[nums.size() - 1] != nums.size())
-            duplicateAndRightValue[1] = nums.size();
-        
-        return duplicateAndRightValue;
+
+        return {duplicate, expectedSum - (actualSum - duplicate)};
     }
 };
---
+++ python/0645.py
@@ -0,0 +1,22 @@
+# Link: https://leetcode.com/problems/set-mismatch/description/
+# Runtime: 166 ms
+# Memory: 18.56 MB
+
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        expected_sum = len(nums) * (len(nums) + 1) // 2
+        actual_sum = sum(nums)
+
+        different_numbers = set()
+        duplicate = 0
+        i, is_found = 0, False
+        while (i < len(nums)) and (not is_found):
+            if nums[i] in different_numbers:
+                duplicate = nums[i]
+                is_found = True
+            
+            different_numbers.add(nums[i])
+
+            i += 1
+        
+        return [duplicate, expected_sum - (actual_sum - duplicate)]
