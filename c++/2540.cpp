504d3e4cd4f08328b465e18e3e0946c97a4456e0
gu1show <denissolovyov57@gmail.com>
2024-03-09 12:55:43 +0300

    2540

---
+++ c++/2540.cpp
@@ -0,0 +1,23 @@
+// Link: https://leetcode.com/problems/minimum-common-value/description/
+// Runtime: 61 ms 
+// Memory: 52.83 MB
+
+class Solution 
+{
+public:
+    int getCommon(const std::vector<int>& nums1, const std::vector<int>& nums2) 
+    {
+        int i = 0, j = 0;
+        while ((i < static_cast<int>(nums1.size())) && (j < static_cast<int>(nums2.size())))
+        {
+            if (nums1[i] == nums2[j])
+                return nums1[i];
+            else if (nums1[i] < nums2[j])
+                i++;
+            else
+                j++;
+        }
+
+        return -1;
+    }
+};
---
+++ python/2540.py
@@ -0,0 +1,16 @@
+# Link: https://leetcode.com/problems/minimum-common-value/description/
+# Runtime: 351 ms
+# Memory: 35.59 MB
+
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        i, j = 0, 0
+        while (i < len(nums1)) and (j < len(nums2)):
+            if nums1[i] == nums2[j]:
+                return nums1[i]
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+        
+        return -1
