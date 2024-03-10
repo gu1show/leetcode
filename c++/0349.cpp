7f8b4d0b64e270ce1f7713cedd13a5d6fbdaa510
gu1show <denissolovyov57@gmail.com>
2024-03-10 10:53:39 +0300

    0349

---
+++ c++/0349.cpp
@@ -0,0 +1,26 @@
+// Link: https://leetcode.com/problems/intersection-of-two-arrays/description/
+// Runtime: 4 ms
+// Memory: 13.22 MB
+
+class Solution 
+{
+public:
+    std::vector<int> intersection(const std::vector<int>& nums1, const std::vector<int>& nums2) 
+    {
+        std::unordered_map<int, int> valueAndFrequency;
+        for (const auto& val : nums1)
+            valueAndFrequency[val]++;
+        
+        std::vector<int> intersectionArray;
+        for (const auto& val : nums2)
+        {
+            if (valueAndFrequency.find(val) != valueAndFrequency.end())
+            {
+                intersectionArray.push_back(val);
+                valueAndFrequency.erase(val);
+            }
+        }
+
+        return intersectionArray;
+    }
+};
---
+++ python/0349.py
@@ -0,0 +1,7 @@
+# Link: https://leetcode.com/problems/intersection-of-two-arrays/description/
+# Runtime: 39 ms
+# Memory: 16.8 MB
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list(set(nums1) & set(nums2))
