88162669c637351543109064b7da9691cc45133b
gu1show <denissolovyov57@gmail.com>
2025-01-03 22:03:24 +0300

    2270

---
+++ c++/2270.cpp
@@ -0,0 +1,26 @@
+// Link: https://leetcode.com/problems/number-of-ways-to-split-array/description/
+// Runtime: 0 ms
+// Memory: 89.32 MB
+
+class Solution 
+{
+public:
+    int waysToSplitArray(const std::vector<int>& nums) 
+    {
+        long long leftSum = 0, rightSum = 0;
+        for (const auto& val : nums)
+            rightSum += val;
+
+        int numberOfWaysToSplitArray = 0;
+        for (int i = 0; i < static_cast<int>(nums.size() - 1); i++)
+        {
+            leftSum += nums[i];
+            rightSum -= nums[i];
+
+            if (leftSum >= rightSum)
+                numberOfWaysToSplitArray++;
+        }
+
+        return numberOfWaysToSplitArray;
+    }
+};
---
+++ python/2270.py
@@ -0,0 +1,17 @@
+# Link: https://leetcode.com/problems/number-of-ways-to-split-array/description/
+# Runtime: 48 ms
+# Memory: 32.34 MB
+
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        left_sum, right_sum = 0, sum(nums)
+
+        number_of_ways_to_split_array = 0
+        for i in range(len(nums) - 1):
+            left_sum += nums[i]
+            right_sum -= nums[i]
+
+            if left_sum >= right_sum:
+                number_of_ways_to_split_array += 1
+        
+        return number_of_ways_to_split_array
