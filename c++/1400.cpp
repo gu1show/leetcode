6c07b46f5e068c4b291e201d695f9601835e473b
gu1show <denissolovyov57@gmail.com>
2025-01-11 23:52:37 +0300

    1400

---
+++ c++/1400.cpp
@@ -0,0 +1,26 @@
+// Link: https://leetcode.com/problems/construct-k-palindrome-strings/description/
+// Runtime: 4 ms
+// Memory: 13.54 MB
+
+class Solution 
+{
+public:
+    bool canConstruct(const std::string& s, int k) 
+    {
+        if (static_cast<int>(s.size()) < k)
+            return false;
+        else if (static_cast<int>(s.size()) == k)
+            return true;
+        
+        std::vector<int> frequencies(26);
+        for (const auto& symbol : s)
+            frequencies[symbol - 'a']++;
+        
+        int numberOfOdds = 0;
+        for (const auto& val : frequencies)
+            if (val % 2)
+                numberOfOdds++;
+        
+        return numberOfOdds <= k;
+    }
+};
