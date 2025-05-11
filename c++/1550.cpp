a39f78d6a8c0601625bf9e618bd0a5551eb7f108
gu1show <denissolovyov57@gmail.com>
2025-05-11 22:04:57 +0300

    1550

---
+++ c++/1550.cpp
@@ -0,0 +1,26 @@
+// Link: https://leetcode.com/problems/three-consecutive-odds/description/
+// Runtime: 0 ms
+// Memory: 11.81 MB
+
+class Solution 
+{
+public:
+    bool threeConsecutiveOdds(const std::vector<int>& arr) 
+    {
+        if (arr.size() < 3)
+            return false;
+        
+        bool isTheFirstOdd = arr[0] % 2 == 1, isTheSecondOdd = arr[1] % 2 == 1, isTheThirdIOdd = arr[2] % 2 == 1;
+        for (int i = 3; i < static_cast<int>(arr.size()); i++)
+        {
+            if (isTheFirstOdd && isTheSecondOdd && isTheThirdIOdd)
+                return true;
+            
+            isTheFirstOdd = isTheSecondOdd;
+            isTheSecondOdd = isTheThirdIOdd;
+            isTheThirdIOdd = arr[i] % 2 == 1;
+        }
+
+        return isTheFirstOdd && isTheSecondOdd && isTheThirdIOdd;
+    }
+};
---
+++ python/1550.py
@@ -0,0 +1,20 @@
+# Link: https://leetcode.com/problems/three-consecutive-odds/description/
+# Runtime: 0 ms
+# Memory: 17.82 MB
+
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        if len(arr) < 3:
+            return False
+
+        is_the_first_odd = arr[0] % 2 == 1
+        is_the_second_odd = arr[1] % 2 == 1
+        is_the_third_odd = arr[2] % 2 == 1
+        for i in range(3, len(arr)):
+            if is_the_first_odd and is_the_second_odd and is_the_third_odd:
+                return True
+            
+            is_the_first_odd, is_the_second_odd = is_the_second_odd, is_the_third_odd
+            is_the_third_odd = arr[i] % 2 == 1
+        
+        return is_the_first_odd and is_the_second_odd and is_the_third_odd
